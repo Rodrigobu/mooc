1ea3b4bc982867db6aad419e7bc68918e4911d58
Rodrigo Araujo <rodrigo.araujo@lemeconsultoria.com>
2017-01-24 16:19:30 -0200

    Contiunar em casa

--- courses/models.py
+++ courses/models.py
@@ -26,6 +26,9 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+    @models.permalink
+    def get_aboslute_url(self):
+        return ('courses:details', (), {'slug':self.slug})
 
     class Meta:
         verbose_name = 'Curso'
